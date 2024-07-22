6d02350a5e44ea59c881e6ac85ddf10872263b24
•• Beemer音楽 <62728136+BeemerCodes@users.noreply.github.com>
2024-07-22 23:28:07 +0000

    main page changed

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,27 @@
 import React from "react";
+import Image from "next/image";
+import image from "../img/construction.jpg";
 
 function Home() {
   return (
     <div>
-      <h1 style={{ color: "#2A4156", textAlign: "center", fontSize: "6em" }}>
+      <h1
+        style={{
+          color: "#1c2e35",
+          textAlign: "center",
+          fontSize: "6em",
+          margin: "0px",
+        }}
+      >
         Hello Word!
       </h1>
       <p
         style={{
-          color: "#2A3641",
+          color: "#17242A",
           textAlign: "center",
           lineHeight: "1.6",
           fontSize: "23px",
+          margin: "10px 0px 0px 0px",
         }}
       >
         A persistência é uma qualidade que pode nos ajudar a superar a
@@ -20,8 +30,30 @@ function Home() {
           "Transportai um punhado de terra todos os dias e fareis uma montanha".
         </span>
       </p>
-
-      <img src="/images/construction.jpg" alt="construction" />
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <Image
+          style={{
+            width: "400px",
+            height: "400px",
+            margin: "0 auto",
+            paddingTop: "30px",
+          }}
+          src={image}
+        />
+        {""}
+      </div>
+      <p
+        style={{
+          textAlign: "center",
+          fontSize: "18px",
+          color: "#17242A",
+          fontStyle: "oblique",
+          margin: "0",
+          paddingTop: "30px",
+        }}
+      >
+        Esta página está em desenvolvimento. Volte em breve. :&#41;
+      </p>
     </div>
   );
 }
